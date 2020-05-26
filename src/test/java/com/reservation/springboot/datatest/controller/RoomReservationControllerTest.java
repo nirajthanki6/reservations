8b8d7ad9343a713f191a9ef05789fbefc3ada6b5
nthanki <niraj_thanki@gap.com>
2020-05-26 15:32:41 -0700

    Controller Test Class

---
+++ src/test/java/com/reservation/springboot/datatest/controller/RoomReservationControllerTest.java
@@ -0,0 +1,55 @@
+package com.reservation.springboot.datatest.controller;
+
+import com.reservation.springboot.data.controller.ReservationController;
+import com.reservation.springboot.data.entity.DateUtils;
+import com.reservation.springboot.data.entity.RoomReservation;
+import com.reservation.springboot.data.service.ReservationService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebMvcTest(ReservationController.class)
+public class RoomReservationControllerTest {
+
+    @MockBean
+    private ReservationService reservationService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void getReservations() throws Exception {
+        String dateString = "2020-01-01";
+        Date date = DateUtils.createDateFromDateString(dateString);
+        List<RoomReservation> roomReservations = new ArrayList<>();
+        RoomReservation roomReservation = new RoomReservation();
+        roomReservation.setFirstName("Niraj");
+        roomReservation.setLastName("Thanki");
+        roomReservation.setDate(date);
+        roomReservation.setGuestId(1);
+        roomReservation.setRoomId(100);
+        roomReservation.setRoomName("Business Suite");
+        roomReservation.setRoomNumber("B1");
+        roomReservations.add(roomReservation);
+        given(reservationService.getRoomReservationsForDate(date)).willReturn(roomReservations);
+
+        this.mockMvc.perform(get("/reservations?2020-01-01")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Thanki, Niraj")));
+
+
+    }
+}
